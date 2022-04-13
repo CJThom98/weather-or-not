154a7d399c0644851e99044f23928cd1b54d01f9
Cory Thompson <cory.thompson98@gmail.com>
2022-04-13 13:02:53 -0500

    adjusting the script to retrieve and present the api info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var submit = document.getElementById("submit");
+var city;
 
 submit.addEventListener("click", function() {
     event.preventDefault();
@@ -12,7 +13,75 @@ submit.addEventListener("click", function() {
     getWeather();
 });
 
-function getWeather(city) {
+function renderForecast(weather) {
+    console.log(weather);
+
+    //converting unixTimeStamp: https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
+
+    let unixTimestamp = weather.current.dt
+
+    const dateObject = new Date(unixTimestamp * 1000); 
+    let humanDateFormat = dateObject.toLocaleString();
+    humanDateFormat = humanDateFormat.split(",");
+    humanDateFormat = humanDateFormat[0];
+
+    let temp = Math.round(((weather.current.temp - 273.15) * 9) / 5 + 32);
+    
+    let windSpeed = weather.current.wind_speed
+
+    let humid = weather.current.humidity
+    
+    let UV = weather.current.uvi
+
+    $("#weatherContainer").append(
+        "<div class='listGroup col-12 col-md-12'>"
+        +   "<h3 class='oneDayTitle'>" + city + " (" + humanDateFormat + ")" + "</h3>"
+        +   "<div class='oneDay'>" + "Time: 12pm " + "</div>"
+        +   "<div class='oneDay'>" + "Temperature: " + temp + " °F" + "</div>"
+        +   "<div class='oneDay'>" + "Humidity: " + humid + "%" + "</div>"
+        +   "<div class='oneDay'>" + "Wind Speed: " + windSpeed + " MPH" + "</div>"
+        +   "<div class='oneDay'>" + "UVI: " + UV + "</div>"
+        +
+        "</div>"
+    )
+
+    for(var i = 0; i < weather.daily.length - 3; i++) {
+        $("#weatherContainer").append(
+            "<div class='listGroup col-12 col-md-12'>"
+            +   
+            +   "<div class='oneDay'>" + "Time: 12pm " + "</div>"
+            +   "<div class='oneDay'>" + "Temperature: " + temp + " °F" + "</div>"
+            +   "<div class='oneDay'>" + "Humidity: " + humid + "%" + "</div>"
+            +   "<div class='oneDay'>" + "Wind Speed: " + windSpeed + " MPH" + "</div>"
+            +   "<div class='oneDay'>" + "UVI: " + UV + "</div>"
+            +
+            "</div>"
+        )
+    }
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+
+    var lat = coordinates[0].lat
+    var lon = coordinates[0].lon
+
+    console.log(lat, lon);
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=82aa2c8af221b40909b689982bed938d`)
+    .then(function(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("Whoops! It seems we can't find that city. Please enter a different one.")
+        }
+    })
+    .then(function(json) {
+        renderForecast(json);
+    })
+};
+
+function getWeather() {
     $("#weatherContainer").empty();
     $("#fiveDay").empty();
     $("#day1").empty();
@@ -21,9 +90,10 @@ function getWeather(city) {
     $("#day4").empty();
     $("#day5").empty();
 
-    var city = document.getElementById("city").value;
+    city = document.getElementById("city").value;
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=82aa2c8af221b40909b689982bed938d=" + city )
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=82aa2c8af221b40909b689982bed938d`)
+
 
     .then(function(response) {
         if (response.ok) {
@@ -33,69 +103,60 @@ function getWeather(city) {
         }
     })
     .then(function(json) {
-        var date = moment().format("MM/DD/YYYY");
+        getForecast(json);
+        // var date = moment().format("MM/DD/YYYY");
 
-        $("#weatherContainer").append(
-            "<div class='listGroup col-12 col-md-12'>"
-            +   "<h3 class='oneDayTitle'>" + json.city.name + " (" + date + ")" + "</h3>"
-            +   "<div class='oneDay'>" + "Time: 12pm " + "</div>"
-            +   "<div class='oneDay'>" + "Temperature: " + json.list[2].main.temp + " °F" + "</div>"
-            +   "<div class='oneDay'>" + "Humidity: " + json.list[2].main.humidity + "%" + "</div>"
-            +   "<div class='oneDay'>" + "Wind Speed: " + json.list[2].wind.speed + " MPH" + "</div>"
-            +
-            "</div>"
-        )
 
-        $("#fiveDay").append(
-            "<div class='col-md-12'>"
-            +
-            "<h4 id='fiveDay'>" + "5-Day Forecast: <br> <span id='time'>[Weather at 12pm]</span>" + "</h4>"
-            + "</div>"
-        );
+        // $("#fiveDay").append(
+        //     "<div class='col-md-12'>"
+        //     +
+        //     "<h4 id='fiveDay'>" + "5-Day Forecast: <br> <span id='time'>[Weather at 12pm]</span>" + "</h4>"
+        //     + "</div>"
+        // );
 
-        $("#day1").append(
-            "<div class='forecast card col s12 m6'>"
-            +   "<div class='card-title'>" + moment().add(1, 'days').format('MM/DD/YYYY') + "</div>"
-            +   "<div class='card-text'>" + "Temp: " + json.list[10].main.temp + " °F" + "</div>"
-            +   "<div class='card-text'>" + "Humidity: " + json.list[10].main.humidity + "%" + "</div>"
-            +
-            "</div>"
-        );
+        // $("#day1").append(
+        //     "<div class='forecast card col s12 m6'>"
+        //     +   "<div class='card-title'>" + moment().add(1, 'days').format('MM/DD/YYYY') + "</div>"
+        //     +   "<div class='card-text'>" + "Temp: " + json.list[10].main.temp + " °F" + "</div>"
+        //     +   "<div class='card-text'>" + "Humidity: " + json.list[10].main.humidity + "%" + "</div>"
+        //     +
+        //     "</div>"
+        // );
 
-        $("#day2").append(
-            "<div class='forecast card col s12 m6'>"
-            +   "<div class='card-title'>" + moment().add(2, 'days').format('MM/DD/YYYY') + "</div>"
-            +   "<div class='card-text'>" + "Temp: " + json.list[18].main.temp + " °F" + "</div>"
-            +   "<div class='card-text'>" + "Humidity: " + json.list[18].main.humidity + "%" + "</div>"
-            +
-            "</div>"
-        );
+        // $("#day2").append(
+        //     "<div class='forecast card col s12 m6'>"
+        //     +   "<div class='card-title'>" + moment().add(2, 'days').format('MM/DD/YYYY') + "</div>"
+        //     +   "<div class='card-text'>" + "Temp: " + json.list[18].main.temp + " °F" + "</div>"
+        //     +   "<div class='card-text'>" + "Humidity: " + json.list[18].main.humidity + "%" + "</div>"
+        //     +
+        //     "</div>"
+        // );
 
-        $("#day3").append(
-            "<div class='forecast card col s12 m6'>"
-            +   "<div class='card-title'>" + moment().add(3, 'days').format('MM/DD/YYYY') + "</div>"
-            +   "<div class='card-text'>" + "Temp: " + json.list[26].main.temp + " °F" + "</div>"
-            +   "<div class='card-text'>" + "Humidity: " + json.list[26].main.humidity + "%" + "</div>"
-            +
-            "</div>"
-        );
+        // $("#day3").append(
+        //     "<div class='forecast card col s12 m6'>"
+        //     +   "<div class='card-title'>" + moment().add(3, 'days').format('MM/DD/YYYY') + "</div>"
+        //     +   "<div class='card-text'>" + "Temp: " + json.list[26].main.temp + " °F" + "</div>"
+        //     +   "<div class='card-text'>" + "Humidity: " + json.list[26].main.humidity + "%" + "</div>"
+        //     +
+        //     "</div>"
+        // );
 
-        $("#day4").append(
-            "<div class='forecast card col s12 m6'>"
-            +   "<div class='card-title'>" + moment().add(4, 'days').format('MM/DD/YYYY') + "</div>"
-            +   "<div class='card-text'>" + "Temp: " + json.list[34].main.temp + " °F" + "</div>"
-            +   "<div class='card-text'>" + "Humidity: " + json.list[34].main.humidity + "%" + "</div>"
-            +
-            "</div>"
-        );
+        // $("#day4").append(
+        //     "<div class='forecast card col s12 m6'>"
+        //     +   "<div class='card-title'>" + moment().add(4, 'days').format('MM/DD/YYYY') + "</div>"
+        //     +   "<div class='card-text'>" + "Temp: " + json.list[34].main.temp + " °F" + "</div>"
+        //     +   "<div class='card-text'>" + "Humidity: " + json.list[34].main.humidity + "%" + "</div>"
+        //     +
+        //     "</div>"
+        // );
 
-        $("#day5").append(
-            "<div class='forecast card col s12 m6'>"
-            +   "<div class='card-title'>" + moment().add(5, 'days').format('MM/DD/YYYY') + "</div>"
-            +   "<div class='card-text'>" + "Temp: " + json.list[39].main.temp + " °F" + "</div>"
-            +   "<div class='card-text'>" + "Humidity: " + json.list[39].main.humidity + "%" + "</div>"
-            +
-            "</div>"
-        );
+        // $("#day5").append(
+        //     "<div class='forecast card col s12 m6'>"
+        //     +   "<div class='card-title'>" + moment().add(5, 'days').format('MM/DD/YYYY') + "</div>"
+        //     +   "<div class='card-text'>" + "Temp: " + json.list[39].main.temp + " °F" + "</div>"
+        //     +   "<div class='card-text'>" + "Humidity: " + json.list[39].main.humidity + "%" + "</div>"
+        //     +
+        //     "</div>"
+        // );
     });
 }
